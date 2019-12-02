42621d543b8f8b1ed70fb9a29ef63f95d35a732f
gflueck <guarin.flueck@epfl.ch>
2019-12-02 18:24:25 +0100

    updated webscraping

--- src/webscraping.py
+++ src/webscraping.py
@@ -6,11 +6,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 DRIVER_PATH = "../chromedriver"
+PARLAMENT_CH = "https://www.parlament.ch"
 
 
-def download_councillor_info():
+def download_councillor_info(start=0, end=355):
     link = "https://www.parlament.ch/de/ratsmitglieder#k=#s="
-    pages = np.arange(355)
+    pages = np.arange(start, end)
     driver = webdriver.Chrome(DRIVER_PATH)
 
     for page in pages:
@@ -18,20 +19,35 @@ def download_councillor_info():
         time.sleep(3)
         with open(f"../ratsmitglieder/file_{page:0>3}.html", "w+") as file:
             file.write(driver.page_source)
+            
+    driver.close()
+
+
+def download_business_items(start=0, end=4593):
+    link = "https://www.parlament.ch/en/ratsbetrieb/suche-curia-vista#k=#s="
+    pages = np.arange(start, end)
+    driver = webdriver.Chrome(DRIVER_PATH)
+
+    for page in pages:
+        driver.get(link + str(page * 10 + 1))
+        time.sleep(3)
+        with open(f"../business_items/file_{page:0>3}.html", "w+") as file:
+            file.write(driver.page_source)
+            
+    driver.close()
+
+
+def read_html(path):
+    with open(path, "r") as file:
+        return BeautifulSoup(file.read(), "html.parser")
 
 
 def councillor_info_df():
-    def read_html(path):
-        with open(path, "r") as file:
-            return BeautifulSoup(file.read(), "html.parser")
-
     def person_items(html):
         return html.find_all("div", class_="person-item")
 
-    COUNCILLOR_IMG_LINK = "https://www.parlament.ch"
-
     def image(person):
-        return COUNCILLOR_IMG_LINK + person.find("img")["src"]
+        return PARLAMENT_CH + person.find("img")["src"]
 
     def name(person):
         return person.find("img")["alt"].strip()
