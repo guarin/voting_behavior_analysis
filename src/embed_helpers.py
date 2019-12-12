1c9f9ae866bea02af9bedcd75d7c82e7402eab6a
mattminder <myfiles@Mattus-MacBook-Pro.local>
2019-12-12 21:42:08 +0100

    Final version

---
+++ src/embed_helpers.py
@@ -0,0 +1,98 @@
+from sklearn.decomposition import PCA
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+
+import numpy as np
+import networkx as nx
+import warnings
+
+
+def do_pca(data, explained_var=.75, return_fits=True):
+    """
+    Rescales variables to have mean zero, then performs PCA.
+    
+    Parameters
+    ----------
+    data:
+        Data to transform. Rows are observations, columns are features
+    explained_var:
+        Retains enough principal components to explain "explained_var" percent
+        of the total variance
+    return_fits:
+        If true, returns the rescale fit and the pca fit together with the principal components.
+        Otherwise returns only principal components.
+    """
+    
+    # Fit scaler and PCA
+    scalefit = StandardScaler(with_std=False).fit(data)
+    scaled = scalefit.transform(data)
+    pcafit = PCA().fit(scaled)
+    
+    # Get nb pcs to retain
+    expl_var = np.cumsum(pcafit.explained_variance_ratio_)
+    nb_retain = np.where(expl_var>.75)[0][0]
+    
+    # Project onto PCs
+    pc = pcafit.transform(scaled)[:,:nb_retain]
+    
+    if return_fits:
+        return pc, scalefit, pcafit
+    else:
+        return pc
+
+
+def explained_variance_plot(pcafit):
+    """
+    For a PCA fit, plots the explained variance ratio versus the number of principal components
+    """
+    expl_var = np.cumsum(pcafit.explained_variance_ratio_)
+    plt.plot(expl_var)
+    plt.ylabel("Explained variance")
+    plt.xlabel("Number Principal Components")
+
+    
+def get_knn_graph(pc, k=10):
+    """
+    Obtains knn_graph, returns networkX weighted graph object.
+    
+    Parameters
+    ----------
+    pc:
+        Principal components for graph imputation
+    k:
+        Number of nearest neighbors to consider
+    """
+    knn_graph = kneighbors_graph(pc, k, mode="connectivity")
+
+    # Create networkx object
+    nx_graph = nx.from_scipy_sparse_matrix(knn_graph)
+    if not nx.is_connected(nx_graph):
+        warnings.warn("Resulting graph is not connected, spectral embedding may give unexpected results")
+    return nx_graph
+
+
+def spectral_embedding(nx_graph, n=15):
+    """
+    Calculates spectral embedding based on normalized Laplacian matrix.
+    
+    Parameters
+    ----------
+    nx_graph:
+        NetworkX graph to analyze
+    n_evals:
+        Number of smallest eigenvalues and corresponding eigenvectors to be computed
+        
+    Returns
+    -------
+    evals:
+        Eigenvalues of spectral embedding
+    evecs:
+        Eigenvectors of spectral embedding
+    """
+    norm_L = nx.normalized_laplacian_matrix(nx_graph)
+    evals, evecs = eigs(norm_L, k=n, which="SM")
+    evals = np.real(evals)
+    evecs = np.real(evecs)
+    return evals, evecs
+
+
---
+++ src/preprocessing_helpers.py
@@ -0,0 +1,40 @@
+from sklearn.impute import SimpleImputer
+import numpy as np
+
+
+def yesno_imputed(leg):
+    """
+    Outputs df with 0 for votes 'no', 1 for votes 'yes' and 
+    mean imputation for all other votes.
+    """
+    yesno = leg.copy()
+    yesno[yesno>1] = np.NaN
+    yesno[yesno<0] = np.NaN
+    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+    return imp.fit_transform(yesno.T).T
+
+def get_politician_attendance(leg):
+    """
+    For every politician in legislature gets the number of times they weren't in 
+    parliament during the vote.
+    """
+    n_votes = leg.shape[0]
+    return ((leg<=3).sum(axis=0) - (leg<0).sum(axis=0))/n_votes
+
+def filter_lazy(leg, ncm, thresh):
+    """
+    Removes all politicians in legislature that were present in less than thresh 
+    percent of the votes.
+    """
+    att = get_politician_attendance(leg).to_numpy()
+ #    print(att)
+ #    print(ncm)
+ #    print(att>thresh)
+    return leg.loc[:,att>thresh], ncm.loc[att>thresh,:]
+
+def filter_impute(leg, ncm, thresh=.5):
+    """
+    Filters politicians that aren't there often, and imputes then with the mean.
+    """
+    no_lazy, ncm = filter_lazy(leg, ncm, thresh)
+    return yesno_imputed(no_lazy), ncm
