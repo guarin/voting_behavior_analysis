e5ecd410ab746a390dd96e8c007f569f408fdda2
gflueck <guarin.flueck@epfl.ch>
2019-12-13 15:01:03 +0100

    fixed graph plots

--- src/plot_helpers.py
+++ src/plot_helpers.py
@@ -6,14 +6,13 @@ def by_legislature(plot_fctn, vals):
     """
     Create a plot per legislature, using the specified plot_function 
     """
-    plt.figure(figsize=(16,4))
+    plt.figure(figsize=(16, 4))
     for i, v in enumerate(vals):
-        plt.subplot(1,3,i+1)
+        plt.subplot(1, 3, i + 1)
         plot_fctn(v)
-        plt.title('Legislature ' + str(i+1))
-        
+        plt.title("Legislature " + str(i + 1))
 
-        
+
 def hv_by_legislature(plot_fctn, vals):
     """
     Create a Hollowviews plot per legislature
@@ -23,6 +22,6 @@ def hv_by_legislature(plot_fctn, vals):
         plt = plot_fctn(v)
         plt = plt.opts(frame_height=300)
         plt = plt.opts(frame_width=250)
-        plots.append(plt.relabel('Legislature ' + str(i+1)))
+        plots.append(plt.relabel("Legislature " + str(i + 1)))
     layout = hv.Layout(plots).opts(shared_axes=False)
     return layout
