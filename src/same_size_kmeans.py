5cbff4767300e21993ed56f7b3cb078cb8310b79
mattminder <myfiles@Mattus-MacBook-Pro.local>
2019-12-15 17:48:35 +0100

    Compatibility with newer networkx, changes in text

--- src/same_size_kmeans.py
+++ src/same_size_kmeans.py
@@ -2,11 +2,15 @@ from sklearn.cluster import KMeans
 import numpy as np
 import networkx as nx
 from scipy.spatial import distance
+
+
 def assign_clusters(points,ind,means,cl_size,nbc):
     """Input constant numbers of points to each clusters"""
+    
     def get_dist(point):
         dist = distance.cdist(np.array([point]), np.array(means), 'euclidean')
         return dist[0],np.amax(dist) - np.amin(dist)
+    
     #Order points by the distance to their nearest cluster minus distance to the farthest cluster
     #(= biggest benefit of best over worst assignment)
     dists = list(map(lambda x: get_dist(x),points))
@@ -32,6 +36,8 @@ def assign_clusters(points,ind,means,cl_size,nbc):
             if not c_clusters:
                 c_clusters = clusters
     return clusters
+
+
 def get_new_means(clusters,points):
     """Computes centroids of clusters"""
     means = []
@@ -41,6 +47,7 @@ def get_new_means(clusters,points):
             mean.append([points[point[0]]])
         means.append(np.mean(mean,axis=0)[0])
     return means
+
 
 def eq_size_kmeans(points,ind,nbc,niter,random_state):
     """Perform variation of kmeans with equal cluster size adapted from an algorithm from
@@ -59,11 +66,14 @@ def eq_size_kmeans(points,ind,nbc,niter,random_state):
         iter_ += 1
     return clusters
 
+
 def per_subgraph(uncon_graph,sub_cluster_num,points,random_state = 0, niter = 100):
     """Computes the same size kmeans variation for each connected sub graph of a graph.
         Each subgraph will have a number of clusters specified in sub_cluster_num
         """
-    clusters_dicts = [eq_size_kmeans(points[list(graph.nodes)],list(graph.nodes),nbc,niter,random_state = random_state) for graph,nbc in zip(nx.connected_component_subgraphs(uncon_graph),sub_cluster_num)]
+    
+    subgraphs = [uncon_graph.subgraph(g) for g in nx.connected_components(uncon_graph) ]
+    clusters_dicts = [eq_size_kmeans(points[list(graph.nodes)],list(graph.nodes),nbc,niter,random_state = random_state) for graph,nbc in zip(subgraphs,sub_cluster_num)]
     cluster_dict = {i:[] for i in range(7)}
     i=0
     for dic in clusters_dicts:
